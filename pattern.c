8d471711fa32296200357bcd92dde0b6c20ce173
lillyratnalu65-wq <lillyratnalu65@gmail.com>
2025-10-30 15:30:09 +0530

    Add files via upload

---
+++ pattern.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+ int main()
+     {
+ 	int i,j;
+ 	for(j=1;j<=4;j++)
+ 	{
+	 
+ 	 for(i=1;i<=4;i++)
+ 	 {
+ 	 	printf(" %d",i);
+ 	 	if(i%4==0)
+ 	 	printf("\n");
+ 	 	
+	  }
+    }	 	 
+	 return 0;
+}
