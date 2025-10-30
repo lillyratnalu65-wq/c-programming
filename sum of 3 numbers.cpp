20cc51278c281e56f49e7e2c89aeb7330ff59c86
lillyratnalu65-wq <lillyratnalu65@gmail.com>
2025-10-30 13:24:30 +0530

    Add files via upload

---
+++ sum of 3 numbers.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c,d;
+	printf("enter a value::");
+	scanf("%d",&a);
+	printf("\n enter b value::");
+	scanf("%d",&b);
+	printf("\n enter c value::");
+	scanf("%d",&c);
+	d=a+b+c;
+	printf("sum of three numbers=%d",d);
+	printf("\n division=%d",d/3);
+	return 0;
+	
+}
