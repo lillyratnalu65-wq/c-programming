5da8299fcbdd9d62de2fe5929c087a5fd9a684bc
lillyratnalu65-wq <lillyratnalu65@gmail.com>
2026-02-27 11:43:23 +0530

    Add files via upload

---
+++ nested_ds.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+struct address
+{
+	char street[10];
+	char city[10];
+	long int pcode;
+};
+struct student
+{
+	int rno;
+	char name[10];
+	float per;
+	struct address a;
+};
+int main()
+{
+	struct student s={101,"raju",79.00,{"aditya","bvvm",534202}};
+	printf("%d %s %f %s %s %ld",s.rno,s.name,s.per,s.a.street,s.a.city,s.a.pcode);
+	return 0;
+}
